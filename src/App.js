2db92e503a27246587b8dd1928819dd2ec06d5ab
ndikumwenayo josue <ndikumwenayojosue@gmail.com>
2024-08-27 11:39:33 +0200

    first_commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className='text-neutral-200 text-3xl bg-inherit'>Tail wind css aplied in react application</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
